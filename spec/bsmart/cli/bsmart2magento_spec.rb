aba7a8c1289341134996b9b8041de09b8776b3a9
Jamie English <jamienglish@gmail.com>
2012-01-20 17:24:20 +0000

    Filled in specs for bsmart2magento.rb

--- spec/bsmart/cli/bsmart2magento_spec.rb
+++ spec/bsmart/cli/bsmart2magento_spec.rb
@@ -12,23 +12,47 @@ module Bsmart
       end
 
       context "when supplied a catalog argument" do
-        it "reads the catalog form the path provided"
+        it "reads the catalog form the path provided" do
+          cli = Bsmart2magento.new('-c assets/ecom_catalog_other.xml')
+          cli.run
+          cli.catalog.products.first.name.should == "Hugo Boss Watch" # or something
+        end
       end
 
       context "when not supplied a catalog argument" do
-        it "reads the catalog from the default path"
+        it "reads the catalog from the default path" do
+          cli = Bsmart2magento.new
+          cli.run
+          cli.catalog.products.first.name.should == "Carnival Rose Quartz & Garnet Necklace"
+        end
       end
 
       context "when supplied a csv argument" do
-        it "outputs a csv file to the location provided"
+        it "outputs a csv file to the location provided" do
+          cli = Bsmart2magento.new('-c assets/update_other.csv')
+          cli.run
+          File.exists?('assets/update_other.csv').should be_true
+        end
       end
 
       context "when not supplied a csv argument" do
-        it "outputs a csv file to the default path"
+        it "outputs a csv file to the default path" do
+          cli = Bsmart2magento.new
+          cli.run
+          File.exists?('assets/update.csv').should be_true
+        end
       end
 
       context "when passed the -d arguement" do
-        it "runs as a daemon"
+        it "runs as a daemon" do
+          cli = Bsmart2magento.new('-d')
+          cli.run
+
+          FileUtils.cp('assets/ecom_catalog.xml', 'catalog.xml')
+          sleep(30.seconds)
+
+          File.exists?('update.csv1').should be_true
+        end
       end
     end
   end
