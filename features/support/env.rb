617a82c68b95031926ddc7cbeef7cfd907d42f31
Jamie English <jamienglish@gmail.com>
2012-04-23 20:10:03 +0100

    Cucumber was breaking on JRuby because Aruba was resetting some ENVs. Setting these manually in env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,14 @@
 require 'aruba/cucumber'
 require_relative 'file_generators'
 
-if RUBY_PLATFORM == 'java'
-  Before do @aruba_timeout_seconds = 10 end
-end
+Aruba.configure do |config|
+  config.before_cmd do |cmd|
+    set_env('JRUBY_OPTS', "-X-C --1.9") # disable JIT since these processes are so short lived
+    set_env('JAVA_OPTS', "") # force jRuby to use client JVM for faster startup times
+  end
+end if RUBY_PLATFORM == 'java'
+
+
+Before do @aruba_timeout_seconds = 10 end if RUBY_PLATFORM == 'java'
 
 World FileGenerators
