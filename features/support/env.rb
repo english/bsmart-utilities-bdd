299e526dc1a24bdf341be850131da6e0ac47abbc
Jamie English <jamienglish@gmail.com>
2012-04-23 16:30:01 +0100

    It does this itself

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,6 @@
 require 'aruba/cucumber'
 require_relative 'file_generators'
 
-ENV['PATH'] = "#{File.expand_path(File.dirname(__FILE__) + '/../../bin')}#{File::PATH_SEPARATOR}#{ENV['PATH']}"
-
 if RUBY_PLATFORM == 'java'
   Before do @aruba_timeout_seconds = 10 end
 end
